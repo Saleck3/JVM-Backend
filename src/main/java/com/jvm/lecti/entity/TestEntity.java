1faf557d72316f2d6f56d1413de1343c56b9dbc9
Santiagofl <sfernandezlopes27@gmail.com>
2024-05-07 20:42:50 -0300

    Fixing dependencies

---
+++ src/main/java/com/jvm/lecti/entity/TestEntity.java
@@ -0,0 +1,21 @@
+package com.jvm.lecti.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "tewst", catalog = "test")
+public class TestEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+}
--- src/test/java/com/jvm/lecti/LectiApplicationTests.java
+++ src/test/java/com/jvm/lecti/LectiApplicationTests.java
@@ -1,13 +1,38 @@
 package com.jvm.lecti;
 
-import org.junit.jupiter.api.Test;
+import com.jvm.lecti.controller.TestController;
+import com.jvm.lecti.entity.TestEntity;
+import com.jvm.lecti.service.TestService;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class LectiApplicationTests {
+import java.util.Optional;
 
-	@Test
-	void contextLoads() {
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class LectiApplicationTests {
+
+	private TestController testController;
+	private TestService testService;
+
+	@Before
+	public void init() {
+		testService = mock(TestService.class);
+		testController = new TestController(testService);
 	}
 
-}
+	@Test
+	public void testController(){
+		Integer id = 1;
+		TestEntity entity = new TestEntity(id);
+
+		when(testService.getTest(id)).thenReturn(Optional.of(entity));
+
+		assertEquals(testService.getTest(id), id);
+	}
+
+	}
