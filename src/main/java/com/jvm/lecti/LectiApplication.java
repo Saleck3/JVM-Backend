850b4469dd1b84ae5cec9f9275eab83d28636061
Santiagofl <sfernandezlopes27@gmail.com>
2024-05-07 18:58:25 -0300

    Creating Spring project

---
+++ src/main/java/com/jvm/lecti/LectiApplication.java
@@ -0,0 +1,13 @@
+package com.jvm.lecti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvm/lecti/LectiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvm.lecti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
