5ca1cf5d29e9d99aa72e9c5a77f31c717ee29672
Saleck3 <aledagonale@gmail.com>
2024-05-27 22:21:54 -0300

    Updated SQL

--- query_lecti_EN.sql
+++ query_lecti_EN.sql
@@ -1,84 +1,248 @@
-CREATE DATABASE LECTI;
-USE LECTI;
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: lecti
+-- ------------------------------------------------------
+-- Server version	8.0.36-0ubuntu0.22.04.1
 
-CREATE TABLE USERS (
-id INT PRIMARY KEY NOT NULL auto_increment,
-first_name VARCHAR(40) NOT NULL,
-last_name VARCHAR(40) NOT NULL,
-password VARCHAR(200) NOT NULL,
-email VARCHAR(40) NOT NULL
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `apple`
+--
+
+DROP TABLE IF EXISTS `apple`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `apple` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(40) NOT NULL,
+  `module_id` int DEFAULT NULL,
+  `max_score` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `module_id` (`module_id`),
+  CONSTRAINT `apple_ibfk_1` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`)
+) ;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `apple`
+--
+
+LOCK TABLES `apple` WRITE;
+/*!40000 ALTER TABLE `apple` DISABLE KEYS */;
+INSERT INTO `apple` VALUES (1,'A',1,0),(2,'E',1,0),(3,'I',1,0),(4,'O',1,0),(5,'U',1,0),(6,'M',1,0),(7,'P',1,0),(8,'B',1,0),(9,'S',1,0),(10,'L',1,0),(11,'LL',1,0),(12,'N',1,0),(13,'D',1,0),(14,'T',1,0),(15,'R',1,0),(16,'RR',1,0),(17,'F',1,0),(18,'Ñ',1,0),(19,'K',1,0),(20,'Y',1,0),(21,'C',1,0),(22,'H',1,0),(23,'V',1,0),(24,'Q',1,0),(25,'J',1,0),(26,'G',1,0),(27,'Z',1,0),(28,'W',1,0),(29,'X',1,0),(30,'CH',1,0),(31,'BL',2,0),(32,'BR',2,0),(33,'CL',2,0),(34,'CR',2,0),(35,'DR',2,0),(36,'FL',2,0),(37,'FR',2,0),(38,'GL',2,0),(39,'GR',2,0),(40,'PL',2,0),(41,'PR',2,0),(42,'TR',2,0),(43,'TL',2,0);
+/*!40000 ALTER TABLE `apple` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `avatar`
+--
+
+DROP TABLE IF EXISTS `avatar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `avatar` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `avatar_name` varchar(40) NOT NULL,
+  `url` varchar(50) DEFAULT NULL,
+  `value` int NOT NULL,
+  PRIMARY KEY (`id`)
 );
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `avatar`
+--
 
-CREATE TABLE PLAYER(
-id INT PRIMARY KEY NOT NULL auto_increment,
-player_name VARCHAR(40) NOT NULL,
-birth_date DATETIME NOT NULL,
-total_crowns INT DEFAULT 0,
-spent_crowns INT DEFAULT 0,
-user_id INT,
-recommended_module INT,
-foreign key (USER_ID) REFERENCES USERS(ID)
+LOCK TABLES `avatar` WRITE;
+/*!40000 ALTER TABLE `avatar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `avatar` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `exercise`
+--
+
+DROP TABLE IF EXISTS `exercise`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `exercise` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `apple_id` int NOT NULL,
+  `parameters` varchar(300) NOT NULL,
+  `exercise_type` enum('letter_ordering','image_selection','image_writing','audio_repeating','text_read','video','worksheets') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `apple_id` (`apple_id`),
+  CONSTRAINT `exercise_ibfk_2` FOREIGN KEY (`apple_id`) REFERENCES `apple` (`id`)
 );
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE AVATAR(
-id INT PRIMARY KEY NOT NULL auto_increment,
-avatar_name VARCHAR(40) NOT NULL,
-url VARCHAR(50),
-value INT NOT NULL
+--
+-- Dumping data for table `exercise`
+--
+
+LOCK TABLES `exercise` WRITE;
+/*!40000 ALTER TABLE `exercise` DISABLE KEYS */;
+INSERT INTO `exercise` VALUES (2,6,'{\"words\": [\"MA\",\"NO\"],\"correctPhrase\": \"MANO\",\"url\":\"\"}','letter_ordering'),(3,6,'{\"words\": [\"MESA\",\"MOTO\",\"MONO\",\"MARCO\"],\"correctWord\": \"MESA\",\"url\": \"ImageURL\"}','image_selection'),(4,6,'{\"correctWord\": \"MONO\",\"url\": \"ImageURL\",\"preSelectedLetters\":[\"N\"]}','image_writing'),(5,6,'{\"url\":\"\"}','video'),(6,6,'{\"url\":\"\"}','worksheets'),(7,6,'{\"correctWord\": \"MOMIA\"}','audio_repeating'),(8,6,'{\"correctWord\": \"MIMA\"}','text_read'),(9,6,'{\"words\": [\"MESA\",\"MOTO\",\"MONO\",\"MARCO\"],\"correctWord\": \"ME\",\"url\": \"ImageURL\",\"label\":\"_ _ SA\"}','image_selection');
+/*!40000 ALTER TABLE `exercise` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `module`
+--
+
+DROP TABLE IF EXISTS `module`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `module` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `description` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
 );
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `module`
+--
 
-CREATE TABLE UNLOCKED_AVATAR(
-player_id INT,
-avatar_id INT,
-foreign key (player_id) REFERENCES PLAYER(ID),
-foreign key (avatar_id) REFERENCES AVATAR(ID)
+LOCK TABLES `module` WRITE;
+/*!40000 ALTER TABLE `module` DISABLE KEYS */;
+INSERT INTO `module` VALUES (1,'Basico'),(2,'Intermedio'),(3,'Avanzado');
+/*!40000 ALTER TABLE `module` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `player`
+--
+
+DROP TABLE IF EXISTS `player`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `player` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `player_name` varchar(40) NOT NULL,
+  `birth_date` datetime NOT NULL,
+  `total_crowns` int DEFAULT '0',
+  `spent_crowns` int DEFAULT '0',
+  `user_id` int DEFAULT NULL,
+  `recommended_module` int DEFAULT NULL,
+  `alias` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `player_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE MODULE(
-id INT PRIMARY KEY NOT NULL auto_increment,
-description VARCHAR(40) NOT NULL
+--
+-- Dumping data for table `player`
+--
+
+LOCK TABLES `player` WRITE;
+/*!40000 ALTER TABLE `player` DISABLE KEYS */;
+INSERT INTO `player` VALUES (1,'saleck\'s child','2022-06-23 00:00:00',12,6,1,NULL,'saleck80');
+/*!40000 ALTER TABLE `player` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `result`
+--
+
+DROP TABLE IF EXISTS `result`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `result` (
+  `my_row_id` bigint unsigned NOT NULL AUTO_INCREMENT /*!80023 INVISIBLE */,
+  `player_id` int DEFAULT NULL,
+  `apple_id` int DEFAULT NULL,
+  `score` int DEFAULT NULL,
+  PRIMARY KEY (`my_row_id`),
+  KEY `player_id` (`player_id`),
+  KEY `apple_id` (`apple_id`),
+  CONSTRAINT `result_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`),
+  CONSTRAINT `result_ibfk_2` FOREIGN KEY (`apple_id`) REFERENCES `apple` (`id`)
 );
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE APPLE(
-id INT PRIMARY KEY NOT NULL auto_increment,
-name VARCHAR(40) NOT NULL,
-module_id INT,
-max_score INT,
-foreign key (module_id) REFERENCES MODULE(id)
+--
+-- Dumping data for table `result`
+--
+
+LOCK TABLES `result` WRITE;
+/*!40000 ALTER TABLE `result` DISABLE KEYS */;
+/*!40000 ALTER TABLE `result` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `unlocked_avatar`
+--
+
+DROP TABLE IF EXISTS `unlocked_avatar`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `unlocked_avatar` (
+  `my_row_id` bigint unsigned NOT NULL AUTO_INCREMENT /*!80023 INVISIBLE */,
+  `player_id` int DEFAULT NULL,
+  `avatar_id` int DEFAULT NULL,
+  PRIMARY KEY (`my_row_id`),
+  KEY `player_id` (`player_id`),
+  KEY `avatar_id` (`avatar_id`),
+  CONSTRAINT `unlocked_avatar_ibfk_1` FOREIGN KEY (`player_id`) REFERENCES `player` (`id`),
+  CONSTRAINT `unlocked_avatar_ibfk_2` FOREIGN KEY (`avatar_id`) REFERENCES `avatar` (`id`)
 );
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE EXERCISE_TYPE(
-id INT PRIMARY KEY NOT NULL auto_increment,
-description VARCHAR(40) NOT NULL
-);
+--
+-- Dumping data for table `unlocked_avatar`
+--
 
-CREATE TABLE EXERCISE(
-id INT PRIMARY KEY NOT NULL auto_increment,
-name VARCHAR(40) NOT NULL,
-exercise_type_id INT,
-apple_id INT,
-parameters VARCHAR(300) NOT NULL,
-foreign key (exercise_type_id) REFERENCES EXERCISE_TYPE(id),
-foreign key (apple_id) REFERENCES APPLE(id)
-);
+LOCK TABLES `unlocked_avatar` WRITE;
+/*!40000 ALTER TABLE `unlocked_avatar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `unlocked_avatar` ENABLE KEYS */;
+UNLOCK TABLES;
 
+--
+-- Table structure for table `users`
+--
 
-CREATE TABLE RESULT(
-player_id INT,
-apple_id INT,
-score INT,
-foreign key (player_id) REFERENCES PLAYER(id),
-foreign key (apple_id) REFERENCES APPLE(id)
-);
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(40) NOT NULL,
+  `last_name` varchar(40) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `users`
+--
 
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Saleck','Gonzalez','{PsMlP92WRzalaIOXG5UiZ6hocAyx9slEpXnL7IFnaSY=}63139bd5292412c3a705b8bef38be05ab1dd900aa27c096d4797a2a871add2f3','saleck@lecti.com');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
-
-
-
-
-
-
+-- Dump completed on 2024-05-27 22:08:33
