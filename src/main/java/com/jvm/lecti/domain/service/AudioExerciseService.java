35e9d46196c997cfbc460a3a281f252f8b2cd074
IvoSpruth <ivospruth1999@gmail.com>
2024-06-08 16:22:49 -0300

    Add Json parser to Audio exercise ai Service and POM dependency

--- src/main/java/com/jvm/lecti/domain/service/AudioExerciseService.java
+++ src/main/java/com/jvm/lecti/domain/service/AudioExerciseService.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import com.google.gson.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -34,7 +35,7 @@ public class AudioExerciseService {
       Transcript transcript;
       ResultAudio ra = new ResultAudio();
       try {
-         expectedText = "el leon ruge";//exerciseDAO.findById(exerciseId).get().getParameters();
+         expectedText = JsonParser.parseString(exerciseDAO.findById(exerciseId).get().getParameters()).getAsJsonObject().get("correctAnswer").getAsString();
          transcript = assemblyAiService.trasncribe(convertToFile(mFile, "exercise" + ".mp3"));
          ra = processResult(transcript, expectedText);
       } catch (Exception e) {
